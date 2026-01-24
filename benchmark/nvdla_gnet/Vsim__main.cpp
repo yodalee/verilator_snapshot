84aecd03ff9473c2d24ef0a856f739c0b16429c9
Yoda Lee <lc85301@gmail.com>
2026-01-24 10:58:10 +0800

    Add time measurement and fst dump in NVDLA_gnet

--- benchmark/nvdla_gnet/Vsim__main.cpp
+++ benchmark/nvdla_gnet/Vsim__main.cpp
@@ -3,37 +3,68 @@
 
 #include "verilated.h"
 #include "Vsim.h"
+#include <chrono>
+#include <iostream>
+#include "verilated_fst_c.h"
+#include <cstdio>
+#include <cstring>
 
 //======================
 
 int main(int argc, char** argv, char**) {
-    // Setup context, defaults, and parse command line
+    using namespace std::chrono;
+    auto tic = high_resolution_clock::now();
+
+    // FST option parsing: last argument must be "1" or "0"
+    bool dump_fst = false;
+    if (argc > 1 && argv[argc-1][0] == '1') {
+        dump_fst = true;
+        --argc;
+    }
+
     Verilated::debug(0);
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-    contextp->traceEverOn(true);
     contextp->commandArgs(argc, argv);
 
-    // Construct the Verilated model, from Vtop.h generated from Verilating
     const std::unique_ptr<Vsim> topp{new Vsim{contextp.get(), ""}};
 
-    // Simulate until $finish
+    // Tracing (fst)
+    VerilatedFstC* tfp = nullptr;
+    if (dump_fst) {
+        Verilated::traceEverOn(true);
+        tfp = new VerilatedFstC;
+        topp->trace(tfp, 99);
+        tfp->open("NVDLAgnet.fst");
+    }
+
     while (VL_LIKELY(!contextp->gotFinish())) {
-        // Evaluate model
         topp->eval();
+        if (tfp) tfp->dump(contextp->time());
         // Advance time
         if (!topp->eventsPending()) break;
         contextp->time(topp->nextTimeSlot());
+
+        if (contextp->time() % 100000 == 0) {
+            std::cout << "Complete cycle: " << contextp->time() << " ps; Elapsed time: " 
+                << duration_cast<seconds>(high_resolution_clock::now() - tic).count() << " s" << std::endl;
+        }
     }
 
     if (VL_LIKELY(!contextp->gotFinish())) {
         VL_DEBUG_IF(VL_PRINTF("+ Exiting without $finish; no events left\n"););
     }
 
-    // Execute 'final' processes
     topp->final();
 
-    // Print statistical summary report
+    if (tfp) {
+        tfp->close();
+        delete tfp;
+    }
+
     contextp->statsPrintSummary();
+
+    auto toc = high_resolution_clock::now();
+    std::cout << duration_cast<microseconds>(toc - tic).count() << "us" << std::endl;
 
     return 0;
 }
